e947318ad7e7c57d8ce0d488dc1def4ea859f3f9
Akinyi <vivianmondi@gmail.com>
2015-05-21 00:14:00 +0300

    Raising a 404 Error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,25 +1,35 @@
-#from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render
+
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+
+#from django.template import RequestContext, loader
 
 from .models import Question
 
 def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	#output =', '.join([p.question_text for p in latest_question_list])
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-		'latest_question_list': latest_question_list,
-		})
-	return HttpResponse(template.render(context))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-
+	# Without the render() shortcut
+	# #output =', '.join([p.question_text for p in latest_question_list])
+	# template = loader.get_template('polls/index.html')
+	# context = RequestContext(request, {
+	# 	'latest_question_list': latest_question_list,
+	# 	})
+	# return HttpResponse(template.render(context))
+	
 	#return HttpResponse(output)
-		
 
 	#return HttpResponse("Hello world.Your are at polls index!")
-
+	
+	
 def detail(request,question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("The Question does not exist")
 	return HttpResponse("You are looking at the question %s ." % question_id)
 
 def results(request, question_id):
