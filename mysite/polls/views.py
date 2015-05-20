e6c3ccdd5d9f8317e861376a0c115db6359df501
Akinyi <vivianmondi@gmail.com>
2015-05-20 23:38:11 +0300

    Created a view file in polls
    Made use Django’s template system to separate the design from Python by creating a template that the view can use.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,23 @@
 #from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello world.Your are at polls index!")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	#output =', '.join([p.question_text for p in latest_question_list])
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {
+		'latest_question_list': latest_question_list,
+		})
+	return HttpResponse(template.render(context))
+
+
+	#return HttpResponse(output)
+		
+
+	#return HttpResponse("Hello world.Your are at polls index!")
 
 def detail(request,question_id):
 	return HttpResponse("You are looking at the question %s ." % question_id)
