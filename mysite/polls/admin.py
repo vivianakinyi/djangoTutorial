375a7ce585ce63af8aec19e99cff0649355212d5
Akinyi <vivianmondi@gmail.com>
2015-05-20 21:51:13 +0300

    added a base_site.html file to the admin template from django

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-#admin.site.register(Question)
+#admin.site.register(Question) 
+class ChoiceInline(admin.TabularInline): #Or StackInline
+	model = Choice
+	extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-	#fields = ['pub_date','question_text']
-
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
-	# fieldsets = [
-	# 	(None,  {'fields':
-	# ['question_text']}), ('Date information', {'fields':['pub_date']}),
+    list_display = ('question_text','pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question,QuestionAdmin)
+#admin.site.register(Choice)
