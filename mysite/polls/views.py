f95c7836c646ab7875ab90c659ad92297122477f
Akinyi <vivianmondi@gmail.com>
2015-05-21 13:38:16 +0300

    Added stylesheet css

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,7 +16,7 @@ class IndexView(generic.ListView):
 
 	def get_queryset(self):
 		"""Return the last five published questions"""
-		return Question.objects.filter(pub_date_lte=timezone.now()).order_by('-pub_date[:5]')
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
